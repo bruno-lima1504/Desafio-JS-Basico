71ac2373ad8211442b39da76bcb1508345ee257f
Bruno Lima <bruno.lima1504@gmail.com>
2022-06-22 16:09:31 -0300

    V00

---
+++ index.js
@@ -0,0 +1,23 @@
+function somar (numero1, numero2){
+    return ( numero1 + numero2)
+}
+let resultadoSoma = somar (2, 5)
+console.log ('A soma dos dois numeros é: '+ resultadoSoma)
+
+function subtrair (numero1, numero2){
+    return (numero1, numero2)
+}
+let resultadoSubtracao = subtrair (2,5)
+console.log('A subtração dos dois numeros é: '+ resultadoSubtracao)
+
+function multiplicar (numero1, numero2){
+    return (numero1 * numero2)
+}
+let resultadoMultiplicacao = multiplicar(2,5)
+console.log('A multiplicação dos dois numeros é: '+ resultadoMultiplicacao)
+
+function dividir (numero1, numero2){
+    return (numero1 / numero2)
+}
+let resultadoDivisao = dividir (2,5)
+console.log('A divisão dos dois numeros é: '+ resultadoDivisao)
